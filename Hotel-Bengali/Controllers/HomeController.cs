a1e047693fcedad683fa1b515c628e649d855c8b
Marisel A. Yapura <ay18yr21@gmail.com>
2023-11-07 23:34:22 -0300

    Agregar archivos de proyecto.

---
+++ Hotel-Bengali/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hotel_Bengali.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Reservas()
+        {
+            ViewBag.Message = "Mi pagina de reservas";
+
+            return View();
+        }
+
+    }
+}
